df490edc9e463bc4b6787b439cb4845281c402d3
Arijit Bhattacharya <arijitbhattacharya33@gmail.com>
2019-09-13 01:32:56 +0530

    Day 1 : creating spring boot application with rest api and customized /error (whitelabel error) handle

---
+++ EmployeeService/src/main/java/com/example/employee/exception/EmployeeServiceExceptionAdvice.java
@@ -0,0 +1,14 @@
+package com.example.employee.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EmployeeServiceExceptionAdvice {
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({EmployeeNotFoundException.class})
+    public void handle(EmployeeNotFoundException e) {}
+}
